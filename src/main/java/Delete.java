1a5701c4f72379bfeb1ebe14aaf0ff953869e300
nicolepristin <pristin.nicole@husky.neu.edu>
2018-04-06 06:00:41 -0400

    Add delete functionality

--- src/main/java/Delete.java
+++ src/main/java/Delete.java
@@ -7,27 +7,15 @@ public class Delete {
         CallableStatement cStmt = conn.prepareCall("{call delete_animal_by_id(?)}");
         cStmt.setString(1, animalId);
 
-        ResultSet res = cStmt.executeQuery();
-        ResultSetMetaData rsmd = res.getMetaData();
-        int columnsNumber = rsmd.getColumnCount();
-
-        // print column names
-        for (int i = 1; i <= columnsNumber; i++) {
-            System.out.print(rsmd.getColumnName(i));
-            System.out.print(",  ");
-        }
-        System.out.println();
-
-        while (res.next()) {
-            for (int i = 1; i <= columnsNumber; i++) {
-                if (i > 1) System.out.print(",  ");
-                String columnValue = res.getString(i);
-                System.out.print(columnValue);
-            }
-            System.out.println("");
-        }
+        cStmt.executeQuery();
+        System.out.println("Check that the following record has no entries to ensure record got deleted: ");
+        Read.getAnimalByProperty(conn, animalId, "animal_by_id");
     }
 
     public static void runDelete(Connection conn, Scanner in) throws SQLException {
+        System.out.print("Please enter the ID of the animal you wish to delete: ");
+        String animalId = in.next();
+
+        deleteAnimal(conn, animalId);
     }
 }
